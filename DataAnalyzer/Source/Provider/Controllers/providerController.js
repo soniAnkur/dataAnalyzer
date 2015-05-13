ccc4c07860edc55b1ef6b4fcddb2c776e1eca859
soniAnkur <me.soni88@gmail.com>
2015-05-13 19:51:38 +0530

    bht saare changes

--- DataAnalyzer/Source/Provider/Controllers/providerController.js
+++ DataAnalyzer/Source/Provider/Controllers/providerController.js
@@ -1,10 +1,8 @@
 define(["require", "exports", "../Services/providerService"], function (require, exports, service) {
     var ProviderController = (function () {
         function ProviderController(stockstoreService, $scope) {
-            //$scope.providersList = '[{"source_code":"UHERO","source_id":3},{"source_code":"NSE","source_id":33},{"source_code":"BCB","source_id":35}]';
             this.stockstoreService = stockstoreService;
             this.$scope = $scope;
-            // var vm = this;
             stockstoreService.getProviders().then(function (response) {
                 $scope.providersList = response;
             });
